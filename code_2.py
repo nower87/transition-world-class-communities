2fa112e377d7716ebdecc25351cd8f070eaca7d3
nower87 <152420431+nower87@users.noreply.github.com>
2024-04-28 11:03:36 +0000

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,29 @@
+    return data;
+    const data = generateRandomData();
+    const data = [];
+}
+}
+function generateRandomData() {
+
+
+function main() {
+    data.forEach(item => {
+        console.log("Random Number: " + item);
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+
+main();
+    for (let i = 0; i < 10; i++) {
+    }            System.out.println("Random Number: " + item);
+
+        }
+        for (int item : data) {
+        int[] data = new int[10];
+    public static void main(String[] args) {
+public class RandomDataGenerator {
+
+        }
+}
+        for (int i = 0; i < 10; i++) {
+    }
+            data[i] = (int) (Math.random() * 100) + 1;
